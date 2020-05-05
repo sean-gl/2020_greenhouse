97411da4d93d79ebaefa015f329c8d28ea47eb49
cabliss <claybliss@gmail.com>
2020-05-05 15:17:36 -0600

    update 1st experiment data prep

---
+++ first_summer_experiment/scripts/clay_R_scripts/data_prep/leaf_thermistor_data_prep.R
@@ -0,0 +1,217 @@
+
+rm(list = ls())
+
+### IMPORTANT: SET SYSTEM TIMEZONE TO GMT, THIS IS REQUIRED FOR CODE TO WORK.
+Sys.setenv(TZ='GMT')
+Sys.getenv('TZ') # make sure it got set
+
+require(dplyr); require(tidyr); require(ggplot2)
+
+wd <- "/home/sean/github/2020_greenhouse/first_summer_experiment/data/leaf_thermistor_data/read_only/"
+setwd(wd)
+
+# read in files
+files <- dir(wd)
+files <- files[grepl('.TXT', files)]
+files
+# Note: For these 2 files I'm not 100% sure if they are assigned to the correct block, but it was 
+# before treatments began anyway, so not super important:
+# "leaf_thermistor_dry_east_08_21-23.TXT" and "leaf_thermistor_wet_west_08_21-23.TXT"
+
+# read in all csv files into a list of df
+datList <- lapply(files, function(x) {
+  d <- read.csv(x, header = F, stringsAsFactors = F)
+  d$file <- x
+  return(d)
+})
+
+# combine list of df into 1 df
+thermdat <- do.call(rbind, datList)
+
+
+### ----- LIST OF DATA QUALITY CHECKS ------
+
+
+### Back to the data...
+
+# Assign block based on file name
+thermdat$block <- NA
+thermdat$block[grepl('wet', thermdat$file)] <- 'W'
+thermdat$block[grepl('moderate', thermdat$file)] <- 'M'
+thermdat$block[grepl('dry', thermdat$file)] <- 'D'
+table(thermdat$block)
+
+head(thermdat)
+
+### Convert to long format after some processing...
+# omit 1st column (this is the reference voltage which we don't need)
+thermdat[,2] <- NULL
+colnames(thermdat)[1:9] <- c('timestamp', 1:8)
+thermdat$timestamp <- as.POSIXct(thermdat$timestamp, format="%Y/%m/%d %H:%M:%S", tz='GMT')
+head(thermdat$timestamp)
+# subtract 1 hour from timestamps to convert from MDT to "GMT"
+thermdat$timestamp <- thermdat$timestamp - 3600
+
+thermLong <- tidyr::gather(thermdat, key = 'thermistor', value = 'temp_C', -c(timestamp, file, block))
+table(thermLong$thermistor)
+
+# add plant ID
+thermLong$plant_id <- NA
+
+thermLong$plant_id[thermLong$block == 'D' & thermLong$thermistor %in% 1:2] <- 'D-2'
+thermLong$plant_id[thermLong$block == 'D' & thermLong$thermistor %in% 3:4] <- 'D-1'
+thermLong$plant_id[thermLong$block == 'D' & thermLong$thermistor %in% 5:6] <- 'D-3'
+thermLong$plant_id[thermLong$block == 'D' & thermLong$thermistor %in% 7:8] <- 'D-4'
+
+thermLong$plant_id[thermLong$block == 'M' & thermLong$thermistor %in% 1:2] <- 'M-2'
+thermLong$plant_id[thermLong$block == 'M' & thermLong$thermistor %in% 3:4] <- 'M-1'
+thermLong$plant_id[thermLong$block == 'M' & thermLong$thermistor %in% 5:6] <- 'M-3'
+thermLong$plant_id[thermLong$block == 'M' & thermLong$thermistor %in% 7:8] <- 'M-4'
+
+thermLong$plant_id[thermLong$block == 'W' & thermLong$thermistor %in% 1:2] <- 'W-2'
+thermLong$plant_id[thermLong$block == 'W' & thermLong$thermistor %in% 3:4] <- 'W-1'
+thermLong$plant_id[thermLong$block == 'W' & thermLong$thermistor %in% 5:6] <- 'W-3'
+thermLong$plant_id[thermLong$block == 'W' & thermLong$thermistor %in% 7:8] <- 'W-4'
+
+# check work
+table(thermLong$plant_id)
+table(thermLong$thermistor, thermLong$plant_id)
+
+# add thermistor position column
+thermLong$position <- NA
+thermLong$position[thermLong$thermistor %in% c(1,3,5,7)] <- 'top'
+thermLong$position[thermLong$thermistor %in% c(2,4,6,8)] <- 'bottom'
+table(thermLong$thermistor, thermLong$position, useNA = 'a')
+table(thermLong$plant_id, thermLong$position, useNA = 'a')
+
+# add thermistor "canopy_position" column 
+# NOTE: This column is redundant with "position" column, since plants were large enough to 
+# place thermistors where they did not need to be moved later on, as in 2nd experiment.
+
+thermLong$canopy_position[thermLong$position=='top'] <- 'upper'
+thermLong$canopy_position[thermLong$position=='bottom'] <- 'lower'
+table(thermLong$canopy_position, useNA = 'a')
+
+
+# convert temperature to numeric
+thermLong$temp_C <- as.numeric(thermLong$temp_C)
+
+# remove "file" column before saving
+thermLong$file <- NULL
+
+
+### --- Fix some data with know issues ----
+
+# this thermistor was broken, and fixed on 8/24 but then wired to the wrong plant! Let's just not use its data, since I'm not
+# even sure from my notes what canopy position it was in, and I think the other plant already had 2 thermistors on
+# it anyway.
+ind <- thermLong$plant_id=='W-1' & thermLong$canopy_position=='upper'
+which(ind)
+with(thermLong, table(plant_id, canopy_position, useNA = 'a'))
+thermLong <- thermLong[!ind, ]
+
+
+### Save raw data in LONG format
+saveRDS(thermLong, "/home/sean/github/2020_greenhouse/first_summer_experiment/data/leaf_thermistor_data/leaf_thermistor_data_raw_compiled_long.rds")
+
+# thermLong <- readRDS("/home/sean/github/2020_greenhouse/first_summer_experiment/data/leaf_thermistor_data/leaf_thermistor_data_raw_compiled_long.rds")
+
+
+
+
+### Do 15-minute aggregation and re-save
+thermLong$by15 <- lubridate::ceiling_date(thermLong$timestamp, unit = '15 minutes')
+aggTherm <- plyr::ddply(thermLong, .variables = c('by15', 'block', 'thermistor',
+                                                  'plant_id', 'position', 'canopy_position'), 
+                        function(x) {
+                          setNames(mean(x$temp_C, na.rm = T), 'mean_leaftemp_C')
+                        })
+### Save aggregated data
+saveRDS(aggTherm, "/home/sean/github/2020_greenhouse/first_summer_experiment/data/leaf_thermistor_data/leaf_thermistor_data_15min_agg.rds")
+
+# aggTherm <- readRDS("/home/sean/github/2020_greenhouse/first_summer_experiment/data/leaf_thermistor_data/leaf_thermistor_data_15min_agg.rds")
+
+
+
+# --- Flag some data that is known to be bad. 
+# NOTE: I checked the thermistors only on 9/3, after spider mites were in full effect.
+# So I have no way of knowing how long thermistor data was bad before 9/3. 
+# I will conservatively assume here that data was ok through 8/28, since I was in the greenhouse on 8/24 and 
+# Garret didn't see spider mites until late on the week of 8/28 (probably Thrusday or Friday)
+
+# set timestamp index
+ind1 <- aggTherm$by15 >= as.POSIXct('2019-08-29 00:00')
+
+# Now, flag individual thermistors:
+# Flags: "ok" = data good; "thermistor bad" = thermistor in bad position; 
+# "leaf dead" = leaf entirely dead, including under thermistor
+# "
+aggTherm$flag <- 0
+
+# D block
+aggTherm$flag[ind1 & aggTherm$plant_id=='D-1' & aggTherm$position=='bottom'] <- 4
+aggTherm$flag[ind1 & aggTherm$plant_id=='D-1' & aggTherm$position=='top'] <- 3
+aggTherm$flag[ind1 & aggTherm$plant_id=='D-2' & aggTherm$position=='bottom'] <- 3
+aggTherm$flag[ind1 & aggTherm$plant_id=='D-2' & aggTherm$position=='top'] <- 4
+aggTherm$flag[ind1 & aggTherm$plant_id=='D-3' & aggTherm$position=='bottom'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='D-3' & aggTherm$position=='top'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='D-4' & aggTherm$position=='bottom'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='D-4' & aggTherm$position=='top'] <- 1
+
+# W block
+aggTherm$flag[ind1 & aggTherm$plant_id=='W-1' & aggTherm$position=='bottom'] <- 4
+aggTherm$flag[ind1 & aggTherm$plant_id=='W-1' & aggTherm$position=='top'] <- 3
+aggTherm$flag[ind1 & aggTherm$plant_id=='W-2' & aggTherm$position=='bottom'] <- 3
+aggTherm$flag[ind1 & aggTherm$plant_id=='W-2' & aggTherm$position=='top'] <- 0
+aggTherm$flag[ind1 & aggTherm$plant_id=='W-3' & aggTherm$position=='bottom'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='W-3' & aggTherm$position=='top'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='W-4' & aggTherm$position=='bottom'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='W-4' & aggTherm$position=='top'] <- 1
+
+# M block
+aggTherm$flag[ind1 & aggTherm$plant_id=='M-1' & aggTherm$position=='bottom'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='M-1' & aggTherm$position=='top'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='M-2' & aggTherm$position=='bottom'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='M-2' & aggTherm$position=='top'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='M-3' & aggTherm$position=='bottom'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='M-3' & aggTherm$position=='top'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='M-4' & aggTherm$position=='bottom'] <- 1
+aggTherm$flag[ind1 & aggTherm$plant_id=='M-4' & aggTherm$position=='top'] <- 2
+
+table(aggTherm$block, aggTherm$flag, useNA = 'a')
+
+
+
+## Examine distribution of temperature data and create flags for obviously high/low temperatures
+plot(density(thermLong$temp_C, na.rm = T), xlim=c(-10, 50))
+hi <- thermLong$temp_C >= 40 & !is.na(thermLong$temp_C)
+low <- thermLong$temp_C <= 10 & !is.na(thermLong$temp_C)
+
+length(which(hi)) /nrow(thermLong) * 100 # 0.6% of observations > 40 deg C
+length(which(low)) /nrow(thermLong) * 100 # 3 % of observations < 10 deg C
+
+thermLong$temperature_flag <- 'none'
+thermLong$temperature_flag[hi] <- '>= 40 C'
+thermLong$temperature_flag[low] <- '<= 10 C'
+
+### Re-save FLAGGED data (both raw and 15-minute re-aggregated)
+thermLong$date <- NULL
+saveRDS(thermLong, "/home/sean/github/2020_greenhouse/first_summer_experiment/data/leaf_thermistor_data/leaf_thermistor_data_raw_compiled_long_flagged.rds")
+
+
+
+# ---- NOTE: Did not run this code, will aggregate data later on after combining ....
+
+
+# # read back in
+# # thermLong <- readRDS("/home/sean/github/2020_greenhouse/first_summer_experiment/data/leaf_thermistor_data/leaf_thermistor_data_raw_compiled_long_flagged.rds")
+# 
+# # Aggregate by block/treatment means
+# by15Flagged <- plyr::ddply(thermLong, .variables = c('by15', 'block', 'thermistor', 'treatment',
+#                                                      'plant_id', 'position', 'canopy_position',
+#                                                      'flag','temperature_flag'), 
+#                            function(x) {
+#                              setNames(mean(x$temp_C, na.rm = T), 'mean_leaftemp_C')
+#                            })
+# 
+# saveRDS(by15Flagged, "/home/sean/github/2020_greenhouse/first_summer_experiment/data/leaf_thermistor_data/leaf_thermistor_data_15min_agg_flagged.rds")
---
+++ first_summer_experiment/scripts/clay_R_scripts/data_prep/scale_data_prep.R
@@ -0,0 +1,191 @@
+# Clay Bliss
+# Script to pre-process scale data from Gleason's 2019 greenhouse experiment.
+# Data is 30-second interval. 
+
+# -----------------------------------
+
+rm(list = ls())
+
+
+### IMPORTANT: SET SYSTEM TIMEZONE TO GMT, THIS IS REQUIRED FOR CODE TO WORK.
+Sys.setenv(TZ='GMT')
+Sys.getenv('TZ') # make sure it got set
+
+# load pacakges
+require(lubridate)
+require(plyr)
+require(tidyr)
+require(ggplot2)
+
+wd <- "/home/sean/github/2020_greenhouse/first_summer_experiment/data/scale_output/read_only/"
+setwd(wd)
+
+###
+### ------Read and process data
+###
+
+### Experiment 1 data ----
+
+# read in files
+setwd(wd)
+files <- dir(wd)
+files <- files[grepl('.csv', files)]
+files
+
+datList <- lapply(files, function(x) read.csv(x, header = F, skipNul = F, stringsAsFactors = F))
+names(datList) <- files
+for(i in 1:length(datList)) {
+  datList[[i]][,4] <- files[i]
+  colnames(datList[[i]]) <- c('scale','timestamp','weight','file')
+  datList[[i]]$timestamp <- as.POSIXct(datList[[i]]$timestamp, format="%Y-%m-%d %H:%M:%S", tz = 'GMT')
+}
+
+### ------Preliminary combine data and check for duplicate rows
+scaledat<- do.call(rbind, datList)
+rownames(scaledat) <- NULL
+
+# NOTE: these duplicate rows will be fixed below...time zone issue.
+dups <- duplicated(scaledat[,c('scale','timestamp')])
+table(scaledat$file[dups]) # a bunch of duplicates in this file, remove them
+scaledat <- scaledat[!dups, ]
+# check
+dups <- duplicated(scaledat[,c('scale','timestamp')])
+any(dups)
+
+### NOTE: Clock was set to MST, change to GMT (no daylight savings) for consistency with 2nd fall experiment
+head(scaledat$timestamp)
+scaledat$timestamp <- scaledat$timestamp - 3600
+head(scaledat$timestamp)
+
+
+# remove rows with no scale number
+which(is.na(scaledat$scale))
+scaledat[is.na(scaledat$scale),] 
+scaledat <- scaledat[!is.na(scaledat$scale), ]
+
+# Order data by timestamp and scale number
+scaledat <- scaledat[order(scaledat$timestamp, scaledat$scale),  ]
+
+
+### ------ Change data types and further data "massaging"
+scaledat$scale <- factor(scaledat$scale)
+
+
+
+# Assign treatments
+# NOTE: These didn't go into effect until 8/24, irrig. at 18:00 MST
+scaledat[scaledat$scale %in% 1:4, 'treatment'] <- 'moderate_drought'
+scaledat[scaledat$scale %in% 5:8, 'treatment'] <- 'full_drought'
+scaledat[scaledat$scale %in% c(9, 10, 12, 14), 'treatment'] <- 'well_watered'
+scaledat$date <- lubridate::date(scaledat$timestamp)
+
+# reset rownames
+rownames(scaledat) <- NULL
+
+
+### Examine data
+ggplot(scaledat, aes(x=timestamp, y=weight, color=scale)) + geom_line() + facet_grid(~treatment)
+
+
+
+# first, add all pot IDs regardless of date.
+table(scaledat$treatment, scaledat$scale)
+scaledat$plant_id <- sapply(as.character(scaledat$scale), function(x) switch(x,
+                                                                             '1'='M-2', '2'='M-1','3'='M-3','4'='M-4',
+                                                                             '5'='D-3','6'='D-4','7'='D-1','8'='D-2',
+                                                                             '9'='W-4','10'='W-1','12'='W-2','14'='W-3'))
+table(scaledat$plant_id, scaledat$scale)
+
+# add block
+scaledat$block <- as.factor(substr(scaledat$plant_id, 1, 1))
+
+
+# Checking irrigation amounts/times
+sub = subset(scaledat, treatment == 'full_drought' & date <= '2019-08-25')
+sub = subset(scaledat, treatment == 'moderate_drought' & date >= '2019-08-20') 
+
+ggplot(sub, aes(x=timestamp, y=weight, color=plant_id)) + geom_line() + scale_x_datetime(breaks = '1 day')
+
+# add minutes column
+scaledat$minutes <- hour(scaledat$timestamp)*60 + minute(scaledat$timestamp)
+
+# calculate irrigation by scale
+irrig <- ddply(scaledat, .(date, block, plant_id, scale), function(x){
+  y <- subset(x, minutes >= 17*60 & minutes <= 19*60)
+  # y <- x
+  setNames(max(y$weight, na.rm = T) - min(y$weight, na.rm = T), 'irrig')
+})
+
+sub = subset(irrig, date >= '2019-08-23')
+ggplot(sub, aes(x=date, y=irrig, color=plant_id)) + geom_line() + geom_point()
+
+sub = subset(irrig, block=='D')
+ggplot(sub, aes(x=date, y=irrig, color=plant_id)) + geom_line() + geom_point()
+
+sub = subset(irrig, block=='W')
+ggplot(sub, aes(x=date, y=irrig, color=plant_id)) + geom_line() + geom_point()
+
+
+#### Save the compiled file before doing more work!
+saveRDS(scaledat, "/home/sean/github/2020_greenhouse/first_summer_experiment/data/scale_output/scale_data_compiled_raw_long.rds")
+
+# read back in
+# scaledat <- readRDS("/home/sean/github/2020_greenhouse/first_summer_experiment/data/scale_output/scale_data_compiled_raw_long.rds")
+
+
+
+### ---- Aggregate (15 minutes) and Flag 
+
+# source the function 'aggAndFlag'
+source("/home/sean/github/2020_greenhouse/second_fall_experiment/scripts/clay_R_scripts/data_prep/flagging_functions.R")
+
+# First, subset data to 8/24 and beyond (start of treatments was 8/24)
+scaledat <- subset(scaledat, date >= '2019-08-24')
+
+# note: this function creates flags 
+aggDat <- aggAndFlag(df = scaledat,
+                     timeCol = 'timestamp', 
+                     idCols = c('scale','plant_id','treatment'),
+                     measCol = 'weight',
+                     threshVec = c(.03, .04, .05),
+                     interval = 15,
+                     diffFn = maxFn)
+table(aggDat$flag)
+
+# rename columns
+colnames(aggDat)[colnames(aggDat) %in% 'mean'] <- 'mean_weight_kg'
+colnames(aggDat)[colnames(aggDat) %in% 'roundTime'] <- 'by15'
+  
+# SAve aggregated and flagged dataframe
+saveRDS(aggDat, "/home/sean/github/2020_greenhouse/first_summer_experiment/data/scale_output/scale_data_long_aggflag.rds")
+
+# read back in
+# aggDat <- readRDS("/home/sean/github/2020_greenhouse/first_summer_experiment/data/scale_output/scale_data_long_aggflag.rds")
+
+### Add "manual" flags
+
+### Manual flag for irrigation 
+
+# add hour and date columns
+aggDat$date <- lubridate::date(aggDat$by15)
+aggDat$hour <- lubridate::hour(aggDat$by15)
+
+
+
+# create flags "irrig1", "irrig2", "irrig3" for 1st 3 hours after irrigation
+# irrigation was at 18:00 each day (MST)
+for(i in 1:3) {
+  aggDat$flag[aggDat$hour == 18+i-1] <- paste0('irrig', i)
+}
+
+table(aggDat$flag)
+
+
+### Finally, save file with all flags
+saveRDS(aggDat, "/home/sean/github/2020_greenhouse/first_summer_experiment/data/scale_output/scale_data_long_aggflag.rds")
+
+
+
+
+
+
